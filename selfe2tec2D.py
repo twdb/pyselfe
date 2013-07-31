87f0ca6cd5bbeb5e1a06c3a3caf4e8eafef7f53d
Solomon Negusse <solomon.negusse@twdb.texas.gov>
2013-07-31 09:04:05 -0500

    test merging

--- selfe2tec2D.py
+++ selfe2tec2D.py
@@ -19,7 +19,7 @@ programname = "selfe2tec2D.py";
 import os
 import struct
 import sys
-sys.path.append('/home/snegusse/')
+sys.path.append('/home/snegusse/test')
 from string import strip
 
 from numpy import *
